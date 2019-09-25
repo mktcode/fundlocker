67e8abbc4d12e33a49defb661ae1bef938e1ab09
mktcode <kontakt@markus-kottlaender.de>
2019-09-25 13:20:26 +0200

    fresh nuxt project, replaced/removed demo content

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-container class="pt-5">
+    <h1>
+      Fundlocker.io
+    </h1>
+    <h2>
+      Lock funds in smart contracts.
+    </h2>
+    <b-button variant="primary">
+      Deposit
+    </b-button>
+    <b-button variant="primary">
+      Withdraw
+    </b-button>
+  </b-container>
+</template>
