bfd397714f0da794cf09e1191f6a69407cc06084
mktcode <kontakt@markus-kottlaender.de>
2019-10-14 16:54:58 +0200

    added icon imports

--- plugins/font-awesome.js
+++ plugins/font-awesome.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSpinner, faChevronDown, faInfo } from '@fortawesome/free-solid-svg-icons'
+import { faSpinner, faChevronDown, faInfo, faKey, faEnvelope, faGlobe, faCalendar, faUniversity, faHashtag, faCheck } from '@fortawesome/free-solid-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faSpinner, faChevronDown, faInfo)
+library.add(faSpinner, faChevronDown, faInfo, faKey, faEnvelope, faGlobe, faCalendar, faUniversity, faHashtag, faCheck)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
