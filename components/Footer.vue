c08ef817611b04bf9e6e9c18abc2db52203ec054
mktcode <kontakt@markus-kottlaender.de>
2019-10-17 01:50:07 +0200

    added alt attribute in img tag

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container class="py-5 text-center text-white small poweredby">
     Powered By<br>
-    <a href="https://transferwise.com"><img src="/transferwise-logo.png"></a>
+    <a href="https://transferwise.com"><img src="/transferwise-logo.png" alt="Transferwise"></a>
   </b-container>
 </template>
