a582e54f3c56a8e859c5ca6a0ad2921f1a2c98c5
mktcode <kontakt@markus-kottlaender.de>
2019-10-17 01:50:41 +0200

    added template for contract download

---
+++ pages/contractTxtTemplate.js
@@ -0,0 +1,32 @@
+export default `
+# FundLocker SmartContract
+(Created: {{date}})
+
+Check your contract's status at:
+{{contractLink}}
+
+# Refunding
+
+Refund Code:
+{{refundCode}}
+
+To get your money refunded, enter this code at:
+{{refundLink}}
+
+# Withdrawal
+
+Withdrawal Link:
+{{withdrawalLink}}
+
+Requirements:
+
+{{requirements}}
+
+# Deposit Details
+
+File the following transfer to deposit money in this contract. Please make sure
+to enter the below information exactly as shown. Especially the amount and
+reference code must match!
+
+{{depositDetails}}
+`
