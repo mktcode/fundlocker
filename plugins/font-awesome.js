1990cca853abfa6918e78184f9e9af9640913d7d
mktcode <kontakt@markus-kottlaender.de>
2019-10-17 08:10:35 +0200

    added icon imports

--- plugins/font-awesome.js
+++ plugins/font-awesome.js
@@ -1,9 +1,39 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSpinner, faChevronDown, faInfo, faKey, faEnvelope, faGlobe, faCalendar, faUniversity, faHashtag, faCheck, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+import {
+  faSpinner,
+  faChevronDown,
+  faInfo,
+  faKey,
+  faEnvelope,
+  faGlobe,
+  faCalendar,
+  faUniversity,
+  faHashtag,
+  faCheck,
+  faExclamationTriangle,
+  faCopy,
+  faUndo,
+  faDonate
+} from '@fortawesome/free-solid-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faSpinner, faChevronDown, faInfo, faKey, faEnvelope, faGlobe, faCalendar, faUniversity, faHashtag, faCheck, faExclamationTriangle)
+library.add(
+  faSpinner,
+  faChevronDown,
+  faInfo,
+  faKey,
+  faEnvelope,
+  faGlobe,
+  faCalendar,
+  faUniversity,
+  faHashtag,
+  faCheck,
+  faExclamationTriangle,
+  faCopy,
+  faUndo,
+  faDonate
+)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
